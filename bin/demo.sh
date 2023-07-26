429ff2f7ddaff4c131ee92b084d82d1a26117ace
Bin Wu <binwu@google.com>
2023-07-26 10:14:04 +0800

    upgraded ECK & GKE

--- bin/demo.sh
+++ bin/demo.sh
@@ -11,8 +11,8 @@ default_pool=default-pool
 nodes_per_zone=5 # per zone
 machine_type=e2-standard-2
 release_channel=None # None -> static, e.g. rapid, regular, stable
-gke_version=1.26.4-gke.500
-eck_version=2.8.0
+gke_version=1.26.5-gke.2700
+eck_version=2.9.0
 es_cluster_name=dingo-demo
 
 __create_gke() {
--- bin/gke.sh
+++ bin/gke.sh
@@ -11,8 +11,8 @@ default_pool=default-pool
 nodes_per_zone=6 # per zone
 machine_type=n2-standard-8
 release_channel=None # None -> static, e.g. rapid, regular, stable
-gke_version=1.26.4-gke.500
-eck_version=2.8.0
+gke_version=1.26.5-gke.2700
+eck_version=2.9.0
 __usage() {
     echo "Usage: ./bin/gke.sh {create|(delete,del,d)|scale|fix}"
 }
